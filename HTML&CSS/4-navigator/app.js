419502c0e3820e233c41efa9e9c19105546a3f0a
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-08-01 12:12:33 +0300

    abort

--- HTML&CSS/4-navigator/app.js
+++
@@ -1,8 +0,0 @@
-let positionLatitude = 10;
-let positionLongitude = 10;
-let destinationLatitude = 23;
-let destinationLongitude = 23;
-let distance = 0;
-
-distance = Math.sqrt((destinationLatitude - positionLatitude)**2 + (destinationLongitude - positionLongitude)**2);
-console.log(Math.round(distance));
