7720c93cc11e6aea937a2a8015ef6e9a4fc5f11b
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-09-03 18:39:12 +0300

    HW

---
+++ HTML&CSS/10-clean-array/app.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5,6,7,8,9];
+
+let checker = (num) => num % 2 === 0 ? true : false;
+
+function cleaner(arr, checker){
+    let result = [];
+    for(let elem of arr){
+        if(checker(elem)){
+            result.push(elem)
+        } 
+    }
+    return result;
+}
+
+console.log(cleaner(arr, checker));
