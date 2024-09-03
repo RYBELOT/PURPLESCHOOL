fd208d9dfb1f8f7cfb07caddc68cd211e98778a8
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-09-03 17:15:16 +0300

    HW

--- HTML&CSS/7-converter/app.js
+++ HTML&CSS/7-converter/app.js
@@ -1,13 +1,7 @@
-let currency = prompt('Выберите валюту EUR/USD', 'usd');
 let sum = prompt('Введите сумму в рублях', 0);
-currency = currency.toLowerCase();
 
-function converter(currency, sum) {
-   if (currency === 'usd') {
+function converterUSD(sum) {
       return sum * 85.95;
-   } else {
-      return currency === 'eur' ? sum * 93.84 : false;
-   }
 }
 
-console.log(converter(currency, sum));
+console.log(converter(sum));
