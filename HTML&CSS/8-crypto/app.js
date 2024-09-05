8dce83e030b252af61a27f0dacca61a9a60b02a1
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-09-05 11:00:24 +0300

    HW

--- HTML&CSS/8-crypto/app.js
+++ HTML&CSS/8-crypto/app.js
@@ -2,18 +2,18 @@ let firstPass = prompt('Укажите новый пароль','password');
 let password = prompt('Введите пароль','password');
 
 function crypto(firstPass) {
-     firstPass = Array.from(firstPass).reverse();
-     firstPass.push(firstPass.shift());
+     firstPass = Array.from(firstPass);
+     let part1 = firstPass.slice(0, Math.floor(firstPass.length/2)).reverse();
+     let part2 = firstPass.slice(Math.floor(firstPass.length/2), firstPass.length).reverse();
+     firstPass = part1.concat(part2);
      return firstPass.toString().replaceAll(',', '');
 }
 
-firstPass = crypto(firstPass);
-
-function check(firstPass,password) {
+function check(firstPass, password) {
     if (!firstPass || !password){
         return false;
     }
-    return firstPass === crypto(password);
+    return crypto(firstPass) === crypto(password);
 }
 
-console.log(`Пароль ${check(firstPass,password)}`);
+console.log(`Зашифрованный пароль: ${crypto(firstPass)} Введенный пароль ${password} Совпадение ${check(firstPass, password)}`);
