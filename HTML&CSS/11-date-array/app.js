8fbdb152e9131137ba9574f4914a8494478f7110
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-09-06 08:52:22 +0300

    correct

--- HTML&CSS/11-date-array/app.js
+++ HTML&CSS/11-date-array/app.js
@@ -1,24 +1,23 @@
 let data = ["01-01-22", "02.02.2321", 123, "34-05-12", "12-24-48", "test"];
 
-console.log(parseDate(data));
+console.log(filterDate(data));
 
-function parseDate(incomungArray) {
-    let resultArr = [];
+function filterDate(incomungArray) {
+    let result = [];
     for (let str of incomungArray) {
-        strToArr = [];
-        Array.from(str).forEach((elem) => strToArr.push(elem * 1));
         let year;
         let month;
         let monthlim;
         let day;
-        if (strToArr.length === 8 || strToArr.length === 10 && strToArr[0] >= 0 &&
-            strToArr[1] >= 0 && strToArr[3] >= 0 && strToArr[4] >= 0 && strToArr[6] >= 0 &&
-            strToArr[7] >= 0 && !(strToArr[2] >= 0) && !(strToArr[5] >= 0)) { // Шаблон по которому проверяем элемент-строку 
-            if (strToArr[8] >= 0 && strToArr[9] >= 0) { //проверяем пришел год в ХХ или ХХХХ формате и берем нужное
-                year = "" + strToArr[8] + strToArr[9];
+        if (str.length === 8 || str.length === 10 && str[0] >= 0 &&
+            str[1] >= 0 && str[3] >= 0 && str[4] >= 0 && str[6] >= 0 &&
+            str[7] >= 0 && !(str[2] >= 0) && !(str[5] >= 0)) { // Шаблон по которому проверяем элемент-строку 
+            if (str[8] >= 0 && str[9] >= 0) { //проверяем пришел год в ХХ или ХХХХ формате и берем нужное
+                year = str[8] + str[9];
             } else {
-                year = "" + strToArr[6] + strToArr[7];
+                year = str[6] + str[7];
             }
+                month = str[3] + str[4];
             if (month == '01' || month == '03' || month == '05' || month == '07' || month == '08' || month == '10' || month == '12') {
                 monthlim = 31; // проверили сколько дней в месяце должно быть
             } else if (month == '02') {
@@ -26,13 +25,12 @@ function parseDate(incomungArray) {
             } else if (month < 12) {
                 monthlim = 30; // проверили сколько дней в месяце должно быть
             }
-            day = "" + strToArr[0] + strToArr[1];
-            if (month <= 12 && day <= monthlim) { // последняя проверка месяц <= 12 дней меньше или равно положенному
-                resultArr.push(`${day}-${month}-${year}`);
+            day = str[0] + str[1];
+            if (month <= 12 && day <= monthlim) { // последние проверки месяц <= 12 b дней меньше или равно положенному
+                result.push(`${day}-${month}-${year}`);
             }
         }
     }
-    return resultArr;
+    return result;
 }
-
 
