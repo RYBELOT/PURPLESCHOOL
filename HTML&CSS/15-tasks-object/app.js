c610d44ba02d835a744f7441590eeda8e9e216cd
Belotserkovskiy Roman <Belotserkovskiy.R.Y@sblogistica.ru>
2024-10-13 08:53:50 +0300

    HW

--- HTML&CSS/15-tasks-object/app.js
+++ HTML&CSS/15-tasks-object/app.js
@@ -1,83 +1,72 @@
 let toDo = {
-  tasks: [],
+  tasks: [{id:2, title:"test1" , priority:20},{id:4, title:"test2", priority:20}],
+  isValid(title, priority) {
+    if (!title || !priority) {
+      console.log('Проверьте заголовок и приоритет');
+      return false;
+    } else if (typeof title === 'string' && priority > 0 && priority <= 9) {
+      return true;
+    } else {
+      console.log('Заголовок должен быть строкой, а приоритет от 1 до 9');
+      return false;
+    }
+  },
+  isIdIndx(id){
+    let result = this.tasks.findIndex((task) => task.id === id);
+    if(result === -1){
+      console.log (`Задача с id ${id} не существует`);
+      return false;
+    }else{
+      return result;
+    }
+  },
   addTask(title, priority) {
-    const check = function (title, priority) {
-      if (!isNaN(priority) && priority >= 1 && priority <= 9 && typeof title === "string") {
-        return true
-      } else {
-        console.log("Приоритет должен быть цифрой, а описание - строкой")
-        return false;
-      }
-    };
-    const output = function (isTrue, arr) {
-      if (isTrue, arr) {
-        arr.forEach((task) => console.log(`id:${task.id} Описание:${task.title} Приоритет:${task.priority}`));
-        return true;
-      } else {
-        console.log("проверьте введеные данные");
-        return false;
-      }
-    };
-    if (check(title, priority)) {
-      let id = this.tasks.length + 1;
+    if(!this.tasks){
+      this.tasks = [];
+    }
+    if (!this.isValid) {
+      return this.isValid
+    } else {
       this.tasks.push({
-        id: id,
+        id: this.lastId() + 1,
         title: title,
         priority: priority
       });
-      return output(true, this.tasks);
-    } else {
-      return output(false, this.tasks);
+      return this.tasks;
     }
   },
-
-  updateTask(id, title, priority) {
-    const isId = function (id, arr) {
-      if (!isNaN(id) && arr.filter((el) => el.id === id)) {
-        return true;
-      } else {
-        console.log("Такого id не существует");
-        return false;
-      }
-    }
-    if (isId(id, this.tasks) && this.addTask.check(title, priority)) {
-      let req = {
-        id: id,
+  updateTask(id,title,priority) {
+    if (this.isIdIndx(id) = -1){
+      return this.isIdIndx(id);
+    } else {
+      let indx = this.isIdIndx(id);
+      let data = {
         title: title,
         priority: priority
-      };
-      tasks.splice(this.tasks.indexOf((el) => el.id === id), 1, req);
-      return this.addTask.output(true, this.tasks);
-    } else {
-      return this.addTask.output(false, this.tasks);
+      }
+      Object.assign(this.tasks[indx], data);
+      return this.tasks
     }
   },
-
   deleteTask(id) {
-    if (this.updateTask.isId(id)) {
-      tasks.splice(this.tasks.indexOf((el) => el.id === id), 1);
-      return this.addTask.output(true, this.tasks);
+    if (this.isIdIndx(id)){
+      this.tasks.splice(this.isIdIndx(id), 1);
+      return this.tasks;
     } else {
-      return this.addTask.output(false, this.tasks);
+      return isIdIndx(id);
     }
   },
-
-  sortTask(order) {
-    if (order === 0 || order === 1) {
-      let sArr = [];
-      order === 0 ? sArr = this.tasks.sort((a, b) => a.priority < b.priority) : sArr = this.tasks.sort((a, b) => a.priority > b.priority);
-      return this.addTask.output(true, sArr);
-    } else {
-      console.log("0 - по убыванию, 1 - по возрастанию");
-      return this.addTask.output(false, this.tasks);
+  sortTask(sortField){
+    if(sortField === "id"){
+      return this.tasks.sort((task1, task2) => task1.id - task2.id);
+    }
+    if(sortField === "priority"){
+      return this.tasks.sort((task1, task2) => task2.priority - task1.priority);
     }
   },
+  lastId(){
+    return this.sortTask("id")[this.tasks.length - 1].id;
+  }
 }
 
-console.log(toDo.addTask("протестить код", 5));
-console.log(toDo.addTask("дважды протестить код", 6));
-console.log(toDo.addTask("трижды код", 4));
-console.log(toDo.addTask("под редактирование", 3));
-console.log(toDo.addTask("под удаление", 8));
-console.log(toDo.updateTask( 4, "редактированное", 9));
 
